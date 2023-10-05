288b15b426bc24dbd2fd82a771dc8dd38437d959
Ali Abd <111752624+aliboss755@users.noreply.github.com>
2023-10-05 08:38:01 +0300

    Setup Dagger hilt

--- app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.plcoding.spotifycloneyt
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.RequestManager
+import com.plcoding.spotifycloneyt.exoplayer.MusicService
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
