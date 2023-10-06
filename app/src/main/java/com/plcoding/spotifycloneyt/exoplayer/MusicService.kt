6ab9bd60c30bc5c52cf02782f38db86c524a719b
Ali Abd <111752624+aliboss755@users.noreply.github.com>
2023-10-06 10:09:10 +0300

    Setting up the Music Notification

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
@@ -8,6 +8,7 @@ import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlayerNotificationListener
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,7 +23,7 @@ class MusicService :MediaBrowserServiceCompat(
 
     @Inject
     lateinit var dataSourceFactory: DefaultDataSourceFactory
-
+    private lateinit var musicNotificationManager: MusicNotificationManager
     @Inject
     lateinit var exoplayer :SimpleExoPlayer
     private  val serviceJob=Job()
@@ -30,7 +31,7 @@ class MusicService :MediaBrowserServiceCompat(
 
     private lateinit var mediaSession:MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
-
+        var isForegroundService =false
     override fun onCreate() {
         super.onCreate()
         val activityIntent =packageManager?.getLaunchIntentForPackage(packageName)?.let {
@@ -43,6 +44,14 @@ class MusicService :MediaBrowserServiceCompat(
 
 
         sessionToken =mediaSession.sessionToken
+
+        musicNotificationManager = MusicNotificationManager(
+            this,
+            mediaSession.sessionToken,
+            MusicPlayerNotificationListener(this)
+        ){
+
+        }
         mediaSessionConnector= MediaSessionConnector(mediaSession)
         mediaSessionConnector.setPlayer(exoplayer)
     }
